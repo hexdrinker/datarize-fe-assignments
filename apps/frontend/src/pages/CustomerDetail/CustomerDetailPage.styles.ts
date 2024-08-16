43b243fcd52a91cd7c2098a8af6dce00bc70099e
Park YuongHo <hexdrinker2020@gmail.com>
2024-08-16 09:33:32 +0900

    feat: 고객 상세 페이지 추가

---
+++ apps/frontend/src/pages/CustomerDetail/CustomerDetailPage.styles.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+const Container = styled.main`
+  width: 400px;
+  margin: 0 auto;
+  text-align: center;
+`
+
+const List = styled.ul`
+  width: 100%;
+`
+
+export default {
+  Container,
+  List,
+}
--- apps/frontend/src/pages/CustomerDetail/index.tsx
+++ apps/frontend/src/pages/CustomerDetail/index.tsx
@@ -1,8 +1,32 @@
+import { useParams } from 'react-router-dom'
+import { useCustomerPurchases } from '../../queries'
+
+import CustomerPurchaseListItem from '../../components/CustomerPurchaseListItem'
+
+import Global from '../../global.styles'
+import Styled from './CustomerDetailPage.styles'
+import ErrorView from '../../components/ErrorView'
+
 const CustomerDetailPage = () => {
+  const { id = '' } = useParams<{ id: string }>()
+  const { data: purchases, isLoading, error, refetch } = useCustomerPurchases(id)
+
+  const refresh = () => {
+    refetch()
+  }
+
   return (
-    <div>
-      <span>customer detail page</span>
-    </div>
+    <Styled.Container>
+      {error && <ErrorView onRefresh={refresh} />}
+      {isLoading && <Global.LoadingSpinner />}
+      {purchases && (
+        <Styled.List>
+          {purchases.map((purchase, index) => (
+            <CustomerPurchaseListItem key={index} {...purchase} />
+          ))}
+        </Styled.List>
+      )}
+    </Styled.Container>
   )
 }
 
